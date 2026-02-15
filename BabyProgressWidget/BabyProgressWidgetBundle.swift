598eea1700bb23fb45ceda652983e2d6bf62c9ef
Pablo Ruiz <p.ruizarnal@gmail.com>
2026-02-15 13:26:29 +0100

    Initial commit

---
+++ BabyProgressWidget/BabyProgressWidgetBundle.swift
@@ -0,0 +1,10 @@
+import WidgetKit
+import SwiftUI
+
+@main
+struct BabyProgressWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        BabyProgressWidget()
+        BabyProgressWidgetControl()
+    }
+}
